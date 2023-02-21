f225271b6b0e709577f6031d496b8e8afafb44e5
Gowthami-20 <121853360+Gowthami-20@users.noreply.github.com>
2023-02-21 15:43:51 +0530

    Add files via upload

---
+++ serialization/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\MALLUGOR\source\repos\console app1\ConsoleApp2\ConsoleApp2.txt";
+            Employee emp = new Employee(201, "Raghav", 80000);
+           FileStream stream = new FileStream(path, FileMode.OpenOrCreate);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(stream, emp);
+            stream.Close();
+            Console.WriteLine("File Created Successfully");
+            Console.ReadKey();
+
+
+
+        
+            stream = new FileStream(path, FileMode.OpenOrCreate);
+             bf = new BinaryFormatter();
+            Employee emp1 = (Employee)bf.Deserialize(stream);
+            Console.WriteLine(emp1.empSalary);
+            stream.Close();
+        }
+    }
+}
---
+++ serialization/class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+
+namespace ConsoleApp3
+{
+    [Serializable]
+    class Employee
+    {
+        public string emName;
+        public int empId;
+        [NonSerialized]
+        public int empSalary;
+
+
+
+
+
+        public Employee(int id, string name, int salary)
+        {
+            this.empId = id;
+            this.emName = name;
+            this.empSalary = salary;
+        }
+
+    }
+}
+
