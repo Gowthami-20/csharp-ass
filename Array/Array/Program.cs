be02ba8e41922f988abac078734b7a072dbb9fca
Gowthami-20 <121853360+Gowthami-20@users.noreply.github.com>
2023-02-21 12:52:19 +0530

    Add files via upload

---
+++ Array/Array/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[7] {1,1,2,2,3,4,5};
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("arr : " + arr[i]);
+            }
+            {
+
+            }
+        }
+    }
+}
