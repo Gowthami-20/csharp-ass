3dd6f8be6911f72ac5eeef9ded8b293868695acb
Gowthami-20 <121853360+Gowthami-20@users.noreply.github.com>
2023-02-21 15:51:44 +0530

    Add files via upload

---
+++ multicasting/publisher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EventExample
+{
+    public  delegate void Cal(int a,int b);
+   public class CalDelegate
+    {
+
+        //events
+        public event Cal Calculator;
+        public void RaiseEvent(int a, int b)
+        {
+            if (Calculator != null)
+            {
+                Calculator(a, b);
+            }
+        }
+    }
+}
