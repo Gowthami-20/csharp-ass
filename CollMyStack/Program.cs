91d684da8794a410b31cc2c57c9154fa212c2a87
Gowthami-20 <121853360+Gowthami-20@users.noreply.github.com>
2023-02-23 09:53:41 +0530

    Add files via upload

---
+++ CollMyStack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CollMyStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
