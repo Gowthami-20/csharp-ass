f33fda628dd4af87800f9315c3d7b9cd7b342900
Gowthami-20 <121853360+Gowthami-20@users.noreply.github.com>
2023-02-23 10:01:49 +0530

    Add files via upload

---
+++ CollMyStack/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace CollMyStack
+{
+    public class MyStack<T>
+    {
+        Stack<T> stack;
+        public void Push(T item)
+        {
+            stack.Push(item);
+        }
+
+        public T Pop()
+        {
+            return stack.Pop();
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> temp = new MyStack<int>();
+
+            temp.Push(1);
+            Console.WriteLine(temp.Pop());
+        }
+    }
+
+}
