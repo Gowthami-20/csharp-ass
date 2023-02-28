90b1329445fe7ae6e81c7e974ab0a15ff05b9440
Gowthami-20 <121853360+Gowthami-20@users.noreply.github.com>
2023-02-28 08:37:07 +0530

    Add files via upload

---
+++ EmpDelegates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
